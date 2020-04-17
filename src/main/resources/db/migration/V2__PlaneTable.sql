1c4cf5a511f17fd9cd985aa6e0c4ac339836ed2e
Linden <las3gk@mail.umkc.edu>
2020-04-17 12:48:45 -0500

    Updated plane model to support pgsql access service methods

---
+++ src/main/resources/db/migration/V2__PlaneTable.sql
@@ -0,0 +1,11 @@
+INSERT INTO Plane (plane_id, tail_number, state, last_action, distance, altitude, speed, heading)
+VALUES (
+'e645a832-80d2-11ea-bc55-0242ac130003',
+'N12345',
+'INBOUND',
+current_timestamp,
+10,
+3000,
+300,
+045
+);
